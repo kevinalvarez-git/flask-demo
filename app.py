37cd18bd7bfa1f1140725fd36a2bd07d01ff60af
Kevin Alvarez <kevin.miner.cs@gmail.com>
2021-07-26 09:12:18 -0700

    First commit, includes working task manager table.

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = None
+        selected     = None
+
+        try:
+            task_content = request.form['task']
+            new_task = Todo(task=task_content)
+        except:
+            selected = request.form.getlist('selected')
+            print(selected)
+
+        if task_content:   
+            try:
+                db.session.add(new_task)
+                db.session.commit()
+                return redirect('/')
+            except:
+                return 'Error at adding task'
+        
+        elif selected:
+            for id in selected:
+                delete(id)
+            return redirect("/")
+        
+        else:
+            return 'Error at processing query'
+
+    else:
+        tasks = Todo.query.order_by(Todo.id).all()
+        return render_template('index.html', tasks = tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error at deleting task', task_to_delete
+
+if __name__ == '__main__':
+    app.run(debug=True)
