07ef1f4c0b4b64175b5ffb4d6b6cb6dede5aad39
Kevin Alvarez <kevin.miner.cs@gmail.com>
2021-07-28 13:28:52 -0700

    Selected queries are now cloned.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # flask-demo
+
+Run 'source env/bin/activate'
+To start application run 'python3 app.py'
+
+On a web browser open localhost:5000
+
+Next steps:
+Update delete route for cloned items.
+Put cloned items on a table.
+
+Add pagination after 10 items.
--- app.py
+++ app.py
@@ -6,20 +6,27 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+# DATABASE MODEL CLASS
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    # RETURN ID OF NEW QUERY CREATED
     def __repr__(self):
         return '<Task %r>' % self.id
 
+# INDEX PAGE ROUTE
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    selected = []
+    clones   = []
     if request.method == 'POST':
         task_content = None
         selected     = None
 
+        # SEE IF A NEW  QUERY WAS ADDED OR IF 
+        # QUERIES WERE SELECTED TO BE CLONED 
         try:
             task_content = request.form['task']
             new_task = Todo(task=task_content)
@@ -27,6 +34,7 @@ def index():
             selected = request.form.getlist('selected')
             print(selected)
 
+        # QUERY ADDED
         if task_content:   
             try:
                 db.session.add(new_task)
@@ -35,18 +43,27 @@ def index():
             except:
                 return 'Error at adding task'
         
-        elif selected:
-            for id in selected:
-                delete(id)
-            return redirect("/")
-        
+        # QUERIES SELECTED TO BE CLONED
+        #elif selected:
+        #    for id in selected:
+        #        delete(id)
+        #    return redirect("/")
+    
         else:
             return 'Error at processing query'
 
+    # DISPLAY ALL RECORDS FROM DATABASE
     else:
         tasks = Todo.query.order_by(Todo.id).all()
-        return render_template('index.html', tasks = tasks)
+        
+        if selected:
+            for task in tasks:
+                if task.id in selected:
+                    clones.append(task)
 
+        return render_template('index.html', tasks = tasks, clones = clones)
+
+# DELETE QUERIES
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
@@ -58,5 +75,6 @@ def delete(id):
     except:
         return 'Error at deleting task', task_to_delete
 
+# MAIN
 if __name__ == '__main__':
     app.run(debug=True)
